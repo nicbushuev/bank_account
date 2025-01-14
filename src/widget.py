0efbe244721ff642d4fcc712369820e2a48b056b
nic_bushuev <nic.funnyhouse@gmail.com>
2025-01-14 22:31:11 +0300

    hot fixes v.2 in widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,30 +1,30 @@
 import re
-
-from masks import get_mask_card_number, get_mask_account
 from datetime import datetime
 
-def mask_account_card(input_bank_information: str) ->str:
-	'''Функция, которая маскирует строковую входящую информацию о карте или счете'''
-
-	try_match = re.search(r'(\d+)$', input_bank_information)
-	if not try_match:
-		raise ValueError('Формат ввода неверный! Введите верный формат Карта/счет номер')
-
-	number = try_match.group(1)
+from masks import get_mask_account, get_mask_card_number
 
 
-	if input_bank_information.lower().startswith(('visa','mastercard','mir','maestro')):
-		masked_number = get_mask_card_number(number)
-	elif input_bank_information.lower().startswith('счет'):
-		masked_number = get_mask_account(number)
-	else:
-		raise ValueError('Неверный формат ввода. Строка должна начинаться с названия платежной системы или слова счет')
+def mask_account_card(input_bank_information: str) -> str:
+    """Функция маскирует строковую входящую информацию о карте или счете"""
 
+    try_match = re.search(r"(\d+)$", input_bank_information)
+    if not try_match:
+        raise ValueError("Формат неверный! Введите формат Карта/счет номер")
 
-	return input_bank_information.replace(number,masked_number)
+    number = try_match.group(1)
+
+    if input_bank_information.lower().startswith(("visa", "mastercard", "mir", "maestro")):
+        masked_number = get_mask_card_number(number)
+    elif input_bank_information.lower().startswith("счет"):
+        masked_number = get_mask_account(number)
+    else:
+        raise ValueError("Неверный формат ввода.")
+
+    return input_bank_information.replace(number, masked_number)
+
 
 def get_date(date_string: str) -> str:
-	"""Преобразует дату в новый формат"""
+    """Преобразует дату в новый формат"""
 
-	date_obj = datetime.fromisoformat(date_string)
-	return f"{date_obj.day:02}.{date_obj.month:02}.{date_obj.year}"
+    date_obj = datetime.fromisoformat(date_string)
+    return f"{date_obj.day:02}.{date_obj.month:02}.{date_obj.year}"
