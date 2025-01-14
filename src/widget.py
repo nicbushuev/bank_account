561f47ef4a33ff70ea74e82f1d00deb8b858ff97
nic_bushuev <nic.funnyhouse@gmail.com>
2025-01-14 22:18:23 +0300

    add func get_date

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
 import re
 
 from masks import get_mask_card_number, get_mask_account
-
+from datetime import datetime
 
 def mask_account_card(input_bank_information: str) ->str:
 	'''Функция, которая маскирует строковую входящую информацию о карте или счете'''
@@ -11,7 +11,7 @@ def mask_account_card(input_bank_information: str) ->str:
 		raise ValueError('Формат ввода неверный! Введите верный формат Карта/счет номер')
 
 	number = try_match.group(1)
-	'''Определяем, является ли входящая банковская информация картой или счетом'''
+
 
 	if input_bank_information.lower().startswith(('visa','mastercard','mir','maestro')):
 		masked_number = get_mask_card_number(number)
@@ -20,5 +20,11 @@ def mask_account_card(input_bank_information: str) ->str:
 	else:
 		raise ValueError('Неверный формат ввода. Строка должна начинаться с названия платежной системы или слова счет')
 
-	'''Вывод результата с замаскированным номером'''
+
 	return input_bank_information.replace(number,masked_number)
+
+def get_date(date_string: str) -> str:
+	"""Преобразует дату в новый формат"""
+
+	date_obj = datetime.fromisoformat(date_string)
+	return f"{date_obj.day:02}.{date_obj.month:02}.{date_obj.year}"
