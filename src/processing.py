2941d89b74d98aab8f9470f0fc1122e7d2b5e99b
nic_bushuev <nic.funnyhouse@gmail.com>
2025-01-22 01:40:55 +0300

    add filtered_by_state func

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
+	'''Функция фильтрует входящий список словарей по ключу "state"'''
+
+	new_filtered_list = []
+	for item in data:
+		if item.get('state') == state:
+			new_filtered_list.append(item)
+	return new_filtered_list
