8b4a0a1becb645c8b7d163958e9b0f8639f8ac94
nic_bushuev <nic.funnyhouse@gmail.com>
2025-01-22 15:48:45 +0300

    fixes in flake8 and processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from src.widget import get_date
+
+
 def filter_by_state(data: list, state: str = "EXECUTED") -> list:
     """
     Функция фильтрует входящий список словарей по значению ключа 'state'
@@ -11,3 +16,25 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
         if item.get("state") == state:
             new_filtered_list.append(item)
     return new_filtered_list
+
+
+def sort_by_date(data: list, decrease: bool = False) -> list:
+    """
+    Функция, создающая новый список словарей с читаемым форматом дат
+    :param data: Список словарей с ключем "data"
+    :param decrease: Сортировка по убыванию - если TRUE, по возрастанию FALSE
+    :return: Новый список словарей с датами в формате ДД.ММ.ГГГГ,
+     отсортированный по дате.
+    """
+
+    new_data = []
+    for item in data:
+        # Копируем словарь, чтобы не менять старый
+        new_item = item.copy()
+        # Преобразуем дату в читаемый формат
+        new_item["date"] = get_date(item["date"])
+        new_data.append(new_item)
+
+    # Сортируем новый список словарей
+    sorted_data = sorted(new_data, key=lambda x: datetime.strptime(x["date"], "%d.%m.%Y"), reverse=decrease)
+    return sorted_data
