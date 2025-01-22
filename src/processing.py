5a97bc11bc9a4bfdd67e8fecb9c820576d97643e
nic_bushuev <nic.funnyhouse@gmail.com>
2025-01-22 12:30:41 +0300

    final fixes in sort_by_date and readme.md

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,10 @@
 def filter_by_state(data: list, state: str = "EXECUTED") -> list:
-    '''Функция фильтрует входящий список словарей по ключу "state"'''
+    """
+    Функция фильтрует входящий список словарей по значению ключа 'state'
+    :param data: Список словарей для фильтрации
+    :param state: Значение ключа 'state', по умолчанию 'EXECUTED'
+    :return: Новый список словарей, где значение ключа 'state' совпадает с указанным
+    """
 
     new_filtered_list = []
     for item in data:
